366f3f8b4ccb6665c4ff791f3717f25125d3ff9a
Pragmatic <info@pragtech.co.in>
2021-06-22 14:29:04 +0530

    update code

--- pragmatic_quickbooks_connector/README.rst
+++
@@ -1,5 +0,0 @@
-==================================
- Odoo Quickbooks Desktop Connector
-==================================
-Add or update your data in quickbooks from Odoo.
-
--- pragmatic_quickbooks_connector/wizard/rescompanymessage.py
+++
@@ -1,6 +0,0 @@
-from odoo import api,fields,models
-
-
-class ResCompanyMessage(models.TransientModel):
-    _name='res.company.message'
-    _description = 'res company message'

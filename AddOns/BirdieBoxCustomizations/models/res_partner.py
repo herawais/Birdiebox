6382741fe1bd6cd3b7cc6133f6402326fc0d6059
camoore <cre.moore@bohmtech.com>
2021-08-13 09:18:53 -0500

    add printer

--- AddOns/BirdieBoxCustomizations/models/res_partner.py
+++ AddOns/BirdieBoxCustomizations/models/res_partner.py
@@ -11,7 +11,21 @@ class CustomResPartner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
-    # x_default_packing_printer = fields.Many2one(
-        
-    #     string='Default Packing Printer'
-    # )
+    x_default_packing_printer = fields.Many2one(
+        comodel_name='res.printers',
+        string='Default Packing Printer',
+        default=False
+    )
+
+    # def __init__(self, pool, cr):
+    #     """ Override of __init__ to add access rights.
+    #         Access rights are disabled by default, but allowed
+    #         on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+    #     """
+    #     readable_fields = [
+    #         'x_default_packing_printer',
+    #     ]
+    #     init_res = super(CustomResPartner, self).__init__(pool, cr)
+    #     # duplicate list to avoid modifying the original reference
+    #     type(self).SELF_READABLE_FIELDS = readable_fields + type(self).SELF_READABLE_FIELDS
+    #     return init_res
--- AddOns/BirdieBoxCustomizations/models/res_users.py
+++ AddOns/BirdieBoxCustomizations/models/res_users.py
@@ -9,13 +9,23 @@ _logger = logging.getLogger(__name__)
 
 class CustomResUsers(models.Model):
     _name = 'res.users'
-    _inherit = ['res.users']
+    _inherit = 'res.users'
 
     # x_default_packing_printer = fields.Many2one(
     #     related='partner_id.x_default_packing_printer',
     #     inherited=True
     # )
 
-    # x_is_alive = fields.Boolean(
-    #     string="Working?"
-    # )
+
+    # def __init__(self, pool, cr):
+    #     """ Override of __init__ to add access rights.
+    #         Access rights are disabled by default, but allowed
+    #         on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+    #     """
+    #     readable_fields = [
+    #         'x_default_packing_printer',
+    #     ]
+    #     init_res = super(CustomResUsers, self).__init__(pool, cr)
+    #     # duplicate list to avoid modifying the original reference
+    #     type(self).SELF_READABLE_FIELDS = readable_fields + type(self).SELF_READABLE_FIELDS
+    #     return init_res
